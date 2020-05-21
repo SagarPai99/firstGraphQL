78f0ac109e3d6bf3022250fc81569b90e6027751
SagarPai99 <sagar.sudesh.pai@gmail.com>
2020-05-22 00:48:37 +0530

    Added Schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,23 +1,24 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 const {GraphQLObjectType,GraphQLString,GraphQLSchema,GraphQLID,GraphQLInt,GraphQLList} = graphql;
-
+const Book = require('../models/Book');
+const Author = require('../models/Author');
 //dummy data
 
-var books = [
-    {name: 'Name of the Wind',genre: 'Fantasy',id:'1',authorId: '1'},
-    {name: 'The Final Empire',genre: 'Fantasy',id:'2',authorId: '2'},
-    {name: 'The Long Earth',genre: 'Sci-Fi',id:'3',authorId: '3'},
-    {name: 'The Hero of Ages',genre: 'Fantasy',id:'4',authorId: '2'},
-    {name: 'The Colour of Magic',genre: 'Fantasy',id:'5',authorId: '3'},
-    {name: 'The Light Fantastic',genre: 'Fantasy',id:'6',authorId: '3'},
-];
+// var books = [
+//     {name: 'Name of the Wind',genre: 'Fantasy',id:'1',authorId: '1'},
+//     {name: 'The Final Empire',genre: 'Fantasy',id:'2',authorId: '2'},
+//     {name: 'The Long Earth',genre: 'Sci-Fi',id:'3',authorId: '3'},
+//     {name: 'The Hero of Ages',genre: 'Fantasy',id:'4',authorId: '2'},
+//     {name: 'The Colour of Magic',genre: 'Fantasy',id:'5',authorId: '3'},
+//     {name: 'The Light Fantastic',genre: 'Fantasy',id:'6',authorId: '3'},
+// ];
 
-var authors = [
-    {name: 'Patrick Rothfuss',age: 44,id:'1'},
-    {name: 'Brandon Sanderson',age: 42,id:'2'},
-    {name: 'Terry Pratchett',age: 66,id:'3'},
-];
+// var authors = [
+//     {name: 'Patrick Rothfuss',age: 44,id:'1'},
+//     {name: 'Brandon Sanderson',age: 42,id:'2'},
+//     {name: 'Terry Pratchett',age: 66,id:'3'},
+// ];
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -84,6 +85,43 @@ const RootQuery = new GraphQLObjectType({
     }
 });
 
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addAuthor: {
+            type: AuthorType,
+            args:{
+                name:{type: GraphQLString},
+                age: {type: GraphQLInt}
+            },
+            resolve(parent,args) {
+                let author = new Author({
+                    name: args.name,
+                    age: args.age
+                });
+                return author.save();
+            }
+        },
+        addBook: {
+            type: BookType,
+            args: {
+                name: {type: GraphQLString},
+                genre: {type: GraphQLString},
+                authorId: {type: GraphQLID}
+            },
+            resolve(parent,args){
+                let book = new Book({
+                    name: args.name,
+                    genre: args.name,
+                    authorId: args.authorId
+                });
+                return book.save();
+            }
+        }
+    }
+});
+
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation: Mutation
 });
