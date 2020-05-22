c12b228c7ced908d9f0d7a77db1ec38c98eface6
SagarPai99 <sagar.sudesh.pai@gmail.com>
2020-05-22 19:09:25 +0530

    Add Book Component

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import {ApolloProvider} from 'react-apollo';
 
 //components
 import BookList from './components/BookList';
-
+import AddBook from './components/AddBook';
 
 //apollo lient setup
 const client = new ApolloClient({
@@ -18,6 +18,7 @@ class App extends Component {
         <div id="main">
           <h1>Sagar's Reading List</h1>
             <BookList />
+            <AddBook />
         </div>
       </ApolloProvider>
     );
